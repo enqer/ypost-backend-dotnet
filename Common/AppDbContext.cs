e17a7be6b7f236d3d1befbb217ae2eec7d4e22cf
enqer <pawel.kam4@gmail.com>
2024-03-07 15:40:25 +0100

    feat: create dbcontext

---
+++ Common/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ypost_backend_dotnet.Entities;
+
+namespace ypost_backend_dotnet.Common
+{
+    public class AppDbContext : DbContext
+    {
+       
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
