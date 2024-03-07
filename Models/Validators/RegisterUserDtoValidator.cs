690529da0ff61153401b591d9fdedbc21497af3b
enqer <pawel.kam4@gmail.com>
2024-03-07 22:02:46 +0100

    feat: validation

---
+++ Models/Validators/RegisterUserDtoValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using ypost_backend_dotnet.Common;
+
+namespace ypost_backend_dotnet.Models.Validators
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
+    {
+        public RegisterUserDtoValidator(AppDbContext dbContext) 
+        {
+   
+            RuleFor(x => x.Password)
+                .MinimumLength(8);
+            RuleFor(x => x.UserName)
+                .MinimumLength(5)
+                .Custom((value, context) =>
+                {
+                    var usernameUsed = dbContext.Users.Any(u => u.UserName ==  value);
+                    if (usernameUsed)
+                    {
+                        context.AddFailure("UserName", "The UserName is taken");
+                    }
+                });
+            RuleFor(x => x.FirstName)
+                .MinimumLength(5);
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .Custom((value, context) =>
+                {
+                    var emailUsed = dbContext.Users.Any(u => u.Email == value);
+                    if (emailUsed)
+                    {
+                        context.AddFailure("Email", "The email is taken");
+                    }
+                });
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ypost_backend_dotnet.Common;
 using ypost_backend_dotnet.Entities;
+using ypost_backend_dotnet.Models;
+using ypost_backend_dotnet.Models.Validators;
 using ypost_backend_dotnet.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
+builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,7 +21,7 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddTransient<Seeder>();
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
-
+builder.Services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
 
 builder.Services.AddDbContext<AppDbContext>(
         option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionString"))
